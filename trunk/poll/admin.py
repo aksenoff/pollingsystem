9ab1cce5de790d9923f73108e2eb6449604cc4b2
aksenoff.a <aksenoff.a@b9aa932b-d608-9e41-215e-8cc7f9b40cd8>
2012-01-15 13:08:30 +0000

    Basic models structure, some admin features implemented

--- trunk/poll/admin.py
+++ trunk/poll/admin.py
@@ -1,1 +1,41 @@
-from poll.models import Pollfrom poll.models import Choicefrom django.contrib import adminclass ChoiceInline(admin.TabularInline):    model = Choice    extra = 3	class PollAdmin(admin.ModelAdmin):    fieldsets = [        (None,               {'fields': ['question']}),        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),    ]    inlines = [ChoiceInline]    list_display = ('question', 'pub_date', 'was_published_today')    list_filter = ['pub_date']    search_fields = ['question']    date_hierarchy = 'pub_date'admin.site.register(Poll, PollAdmin)
+﻿from poll.models import Poll
+from poll.models import Choice
+from poll.models import Question
+from poll.models import Voting
+from poll.models import Ticket
+from django.contrib import admin
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 3
+    
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['caption']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [QuestionInline]
+    list_display = ('caption', 'pub_date', 'was_published_today')
+    list_filter = ['pub_date']
+    search_fields = ['caption']
+    date_hierarchy = 'pub_date'
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['caption']}),
+        ('Options', {'fields': ['multiple_choice', 'number', 'is_optional']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('poll', 'number', 'caption', 'is_optional')
+    list_filter = ['poll__caption']
+    search_fields = ['caption']
+    #date_hierarchy = 'pub_date'
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Voting)
+admin.site.register(Ticket)
--- trunk/settings.py
+++ trunk/settings.py
@@ -13,9 +13,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'c:/programming/pollingsystem/db/db.db3',                      # Or path to database file if using sqlite3.
-        'USER': 'xnv',                      # Not used with sqlite3.
-        'PASSWORD': '314159',                  # Not used with sqlite3.
-        'HOST': 'mysql.alwaysdata.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
