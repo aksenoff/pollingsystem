a58c9d6232540e780800f4065d0dc5b3956a9a4b
aksenoff.a <aksenoff.a@b9aa932b-d608-9e41-215e-8cc7f9b40cd8>
2012-05-10 11:06:30 +0000

    Statistics improved. Errors fixed.

--- trunk/poll/views.py
+++ trunk/poll/views.py
@@ -1,22 +1,22 @@
 # Create your views here.
 # encoding: utf8
-from poll.models import Poll, Choice, Ticket
+from poll.models import Poll, Choice, Ticket, Voting
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
     
-def vote(request, voting_id, ticket):
-    voting = get_object_or_404(Voting, pk=poll_id)
+def vote(request, voting_id):
+    voting = get_object_or_404(Voting, pk=voting_id)
     try:
         selected_choices = {}
-        for question in p.question_set.all():
+        for question in voting.poll.question_set.all():
         	selected_choices[question.number] = int(request.POST['choice'+unicode(question.number)])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the poll voting form.
        return render_to_response('poll/detail.html', {
-            'poll': p,
-            'error_message': "You didn't select a choice.",
+            'voting': voting,
+            'error_message': "Вы неправильно проголосовали",
         }, context_instance=RequestContext(request))
     else:
     	for question in selected_choices.keys():
@@ -24,7 +24,7 @@ def vote(request, voting_id, ticket):
 #        # Always return an HttpResponseRedirect after successfully dealing
 #        # with POST data. This prevents data from being posted twice if a
 #        # user hits the Back button.
-        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('poll.views.enter_ticket'))
 
 def enter_ticket(request):
 	return render_to_response('poll/enter_ticket.html', context_instance=RequestContext(request))
@@ -40,3 +40,9 @@ def poll_select(request):
 		return render_to_response('poll/select.html', {
 			'ticket': my_ticket, 'votings': my_ticket.poll.voting_set.filter(closed=False)
 			})
+
+def stats(request):
+    polls = Poll.objects.all()
+    return render_to_response('poll/stats.html', {
+        'polls': polls,
+        })
