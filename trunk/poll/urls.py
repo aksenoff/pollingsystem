d7320a3c23b89200eba6de2573f36d3e8eb41d0e
aksenoff.a <aksenoff.a@b9aa932b-d608-9e41-215e-8cc7f9b40cd8>
2012-08-12 19:12:03 +0000

    First full-featured version. Authorization mechanism implemented. Ticket is now stored in session and is valid for one voting. Can be cleared. Starting, stopping votings, statistics viewing now require logging in. Results for individual votings are shown (as well as ones with voters' names). Ticket generation mechanism implemented. Ticket entering + voting selection are now on a single url.

--- trunk/poll/urls.py
+++ trunk/poll/urls.py
@@ -1,40 +1,20 @@
 from django.conf.urls.defaults import *
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView
 from poll.models import Poll, Voting
 
 urlpatterns = patterns('',
-    url(r'^$', 'poll.views.enter_ticket'),
-    url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=Voting,
-            template_name='poll/detail.html')),
-    url(r'stats/$',
-        ListView.as_view(
-            model=Poll,
-            template_name='poll/stats.html')),
-    url(r'^stats/(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=Poll,
-            template_name='poll/poll_stats.html'),
-        name='poll_results'),
-    url(r'^stats/(?P<pk>\d+)/tickets/$',
-        DetailView.as_view(
-            model=Poll,
-            template_name='poll/poll_stats_tickets.html')),
-    url(r'^stats/voting/(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=Voting,
-            template_name='poll/voting_stats.html')),
-    url(r'^stats/voting/(?P<pk>\d+)/tickets/$',
-        DetailView.as_view(
-            model=Voting,
-            template_name='poll/voting_stats_tickets.html')),
+    url(r'^$', 'poll.views.poll_select'),
+    url(r'^(?P<voting_id>\d+)/$', 'poll.views.display_voting'),
+    url(r'stats/$', 'poll.views.stats'),
+    url(r'^stats/(?P<poll_id>\d+)/$', 'poll.views.poll_stats'),
+    url(r'^stats/(?P<poll_id>\d+)/tickets/$', 'poll.views.poll_stats_tickets'),
+    url(r'^stats/voting/(?P<voting_id>\d+)/$', 'poll.views.voting_stats'),
+    url(r'^stats/voting/(?P<voting_id>\d+)/tickets/$','poll.views.voting_stats_tickets'),
     url(r'^(?P<voting_id>\d+)/vote/$', 'poll.views.vote'),
-    url(r'^select/$', 'poll.views.poll_select'),
-    url(r'^start/$', 
-        ListView.as_view(
-            model=Poll,
-            template_name='poll/start_panel.html')),
+    url(r'^clear_ticket/$', 'poll.views.clear_ticket'),
+    url(r'^start/$', 'poll.views.poll_list'),
+    url(r'^stop/$', 'poll.views.votings_in_progress_list'),
     url(r'^start/(?P<poll_id>\d+)/$', 'poll.views.start_voting'),
     url(r'^stop/(?P<voting_id>\d+)/$', 'poll.views.stop_voting'),
+    url(r'^generate/', 'poll.views.generate_tickets'),
 )
